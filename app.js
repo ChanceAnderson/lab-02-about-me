7a2f99ed104e353ba62fda9f44ae3da0154030a0
Chance Anderson <chance.cea.anderson@gmail.com>
2017-04-04 13:00:13 -0700

    js written

--- app.js
+++ app.js
@@ -7,3 +7,49 @@ if (sushi.toLowerCase() == 'yes'){
 } else {
   alert('Please type Yes or No');
 };
+
+console.log('Do I like sushi?');
+
+var food = prompt('Do you think I like eating?');
+if (food.toLowerCase() == 'yes'){
+  alert('You were correct!');
+} else if (food.trim().toLowerCase() == 'no'){
+  alert('Incorrect! I love eating!');
+} else {
+  alert('Please type Yes or No');
+};
+
+console.log('Do I like to eat?');
+
+var dogs = prompt('Do you think I like dogs?');
+if (dogs.toLowerCase() == 'yes'){
+  alert('You were correct!');
+} else if (dogs.trim().toLowerCase() == 'no'){
+  alert('Incorrect! I love dogs!');
+} else {
+  alert('Please type Yes or No');
+};
+
+console.log('Do I like dogs?');
+
+var tv = prompt('Do you think I like TV?');
+if (tv.toLowerCase() == 'yes'){
+  alert('Wrong!');
+} else if (tv.trim().toLowerCase() == 'no'){
+  alert('Correct! I do not like tv!');
+} else {
+  alert('Please type Yes or No');
+};
+
+console.log('Do I like watching tv?');
+
+var code = prompt('Do you think I like to code?');
+if (code.toLowerCase() == 'yes'){
+  alert('You were correct! Coding is fun!');
+} else if (code.trim().toLowerCase() == 'no'){
+  alert('Incorrect! I love to code!');
+} else {
+  alert('Please type Yes or No');
+};
+
+console.log('Do I like to code?');
